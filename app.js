91da0062fa76a8f23e4c2a2ad0eece9769d16495
Leonardo Avincetto <lylcomputacion@live.com>
2021-11-14 23:17:59 -0300

    modificado

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const express = require('express')
 const app = express();
+const path = require ("path");
+
+const publicPath = path.resolve(__dirname,'./public');
+app.listen(process.env.PORT || 3000,()=>console.log (`servidor escuchando puerto ${3000}`));
 app.use(express.static('public'));
 
-
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Servidor funcionando');
-});
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/index.html'));
 })
-
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname , './views/home.html');
+    res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname , './views/login.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname , './views/register.html');
+    res.sendFile(__dirname + '/views/register.html');
 });
-
-
